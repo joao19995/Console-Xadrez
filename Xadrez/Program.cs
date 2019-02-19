d84168ec08ee7d30234fbd7f4048c6d24b358df5
Joao Brandao <joaopedro.19995@gmai.com>
2019-02-19 16:33:49 +0000

    Add tabuleiro

---
+++ Xadrez/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Tabuleiro;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro.Tabuleiro tab = new Tabuleiro.Tabuleiro(8, 8);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Xadrez/Tabuleiro/Peca.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuleiro
+{
+    class Peca
+    {
+        
+
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtMovimentos { get; set; }
+        public Tabuleiro tabuleiro { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            this.posicao = posicao;
+            this.cor = cor;           
+            this.tabuleiro = tabuleiro;
+            this.qtMovimentos = 0;
+        }
+
+    }
+}
