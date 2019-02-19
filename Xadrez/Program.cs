813cfdc14219f0739b9dc37c5cbef238ac4e3d93
Joao Brandao <joaopedro.19995@gmai.com>
2019-02-19 19:46:22 +0000

    peca de xadrez

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Tabuleiro;
-using Xadrez.xadrez;
+using xadrez;
 
 namespace Xadrez
 {
@@ -8,20 +8,9 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro.Tabuleiro tab = new Tabuleiro.Tabuleiro(8, 8);
-
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 1));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(6, 0));
-
-                Tela.ImprimirTabuleiro(tab);
-            }catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-           
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toposicao());
 
             Console.ReadLine();
         }
