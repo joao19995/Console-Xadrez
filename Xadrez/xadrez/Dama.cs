232f04c4a3e33aff0582bb4305f7d864faae88af
Joao Brandao <joaopedro.19995@gmai.com>
2019-02-20 23:45:30 +0000

    novas pecas

--- Xadrez/xadrez/Dama.cs
+++ Xadrez/xadrez/Dama.cs
@@ -10,7 +10,7 @@ namespace xadrez
         }
         public override string ToString()
         {
-            return "B";
+            return "D";
         }
 
         private bool podeMover(Posicao pos)
